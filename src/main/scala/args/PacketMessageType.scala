fc33390c175da09efb588c16b173c40f0a120727
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-15 20:17:09 +0100

    Add handling for FrameResolution

--- src/main/scala/args/PacketMessageType.scala
+++ src/main/scala/args/PacketMessageType.scala
@@ -13,6 +13,7 @@ class PacketMessageType(msgType: MessageType.Value) extends ForgeArgument {
       case MessageType.GsnNodeInfo => "--msg-type gsn_node_info"
       case MessageType.GsnStateUpdate => "--msg-type gsn_state_update"
       case MessageType.GsnNodeStatus => "--msg-type gsn_node_status"
+      case MessageType.GsnResolution => "--msg-type gsn_resolution"
         
       case MessageType.GtnGeosubNodes => "--msg-type gtn_geosub_nodes"
       case MessageType.GtnRegistration => "--msg-type gtn_registration"
--- src/main/scala/enums/MessageType.scala
+++ src/main/scala/enums/MessageType.scala
@@ -4,6 +4,7 @@ object MessageType extends Enumeration {
     type MessageType = Value
     val GsnArea, GsnTypeRequest, GsnNodeInfo, 
       GsnRegistration, GsnNodeStatus, GsnStateUpdate,
+      GsnResolution,
       GtnGeosubNodes, GtnRegistration,
       Undefined, UnitTest, Default = Value
 }
